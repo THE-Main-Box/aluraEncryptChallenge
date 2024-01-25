f1207f2e5a769c111b9e519d84049f592baa9224
Thiago de Almeida Barbosa <curiosidades381@gmail.com>
2024-01-24 21:25:21 -0300

    término do estudo do código e pequenas otimizações

--- JS/script.js
+++ JS/script.js
@@ -1,80 +1,75 @@
-let bool = false
-let textarea = document.querySelector('#ta')
-
-const change = () =>{ // criptografar
-    let toEncrypt = document.querySelector('input[name="decText"]').value;
-    bool = toEncrypt.trim() !== '';
-    attClasse()
-
-    document.querySelector('input[name="decText"]').focus()
-    if(bool){
-        textarea.value = Encrypt(toEncrypt)
-    }else{
-        return
-    }
-
-}
+let bool = false;
+let textarea = document.querySelector("#ta");
 
 const substitutionMap = {
-    'a': 'ai',
-    'e': 'enter',
-    'i': 'imes',
-    'o': 'ober',
-    'u': 'ufat'
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
 };
 
 const Encrypt = (texto) => {
-    // Substituição usando o mapeamento
-    texto = texto.replace(/[aeiou]/g, match => substitutionMap[match]);
-    return texto;
-}
+  // Substituição usando o mapeamento
+  texto = texto.replace(/[aeiou]/g, (match) => substitutionMap[match]);
+  return texto;
+};
 
 const Desencrypt = (texto) => {
-    // Inversão da substituição usando o mapeamento
-    texto = texto.replace(/(ai|enter|imes|ober|ufat)/g, match => {
-        for (const key in substitutionMap) {
-            if (substitutionMap[key] === match) {
-                return key;
-            }
-        }
-        return match;
-    });
-    return texto;
-}
-
-
-
-const attClasse = () =>{
-    let message= document.querySelectorAll('.message');
-    message.forEach((messages)=>{
-        messages.classList.toggle(`true`, bool)
-    })
-}
-
-
-const traduzir = () => { // descriptografar
-    let toDesEncrypt = document.querySelector('input[name="decText"]').value;
-    bool = toDesEncrypt.trim() !== '';
-    attClasse()
-
-    document.querySelector('input[name="decText"]').focus()
-    
-    if(bool){
-        textarea.value = Desencrypt(toDesEncrypt)
-    }else{
-        return
+  // Inversão da substituição usando o mapeamento
+  texto = texto.replace(/(ai|enter|imes|ober|ufat)/g, (match) => {
+    for (const key in substitutionMap) {
+      if (substitutionMap[key] === match) {
+        return key;
+      }
     }
-}
+    return match;
+  });
+  return texto;
+};
+
+const attClasse = () => {
+  let message = document.querySelectorAll(".message");
+  message.forEach((messages) => {
+    messages.classList.toggle(`true`, bool);
+  });
+};
+
+const change = () => {
+  // criptografar
+  let toEncrypt = document.querySelector('input[name="decText"]');
+  bool = toEncrypt.value.trim() !== "";
+  attClasse();
+
+  toEncrypt.focus();
+  if (bool) {
+    textarea.value = Encrypt(toEncrypt.value);
+  } else return;
+};
+
+const traduzir = () => {
+  // descriptografar
+  let toDesEncrypt = document.querySelector('input[name="decText"]').value;
+  bool = toDesEncrypt.trim() !== "";
+  attClasse();
+
+  document.querySelector('input[name="decText"]').focus();
+
+  if (bool) {
+    textarea.value = Desencrypt(toDesEncrypt);
+  } else {
+    return;
+  }
+};
 
 function copiarTexto() {
-    if(bool){
-    let inputElement = document.querySelector('#ta');
+  if (bool) {
+    let inputElement = document.querySelector("#ta");
+    document.querySelector('input[name="decText"]').value = ''
 
     inputElement.select();
     document.execCommand("copy");
 
-    inputElement.blur();
-
     alert("Texto copiado para a área de transferência!");
-    } else return
+  } else return;
 }
